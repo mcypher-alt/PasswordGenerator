2639b3b165d1ea7c618a79660bb00e7b8e01c764
mcypher-alt <nikich-varganov@mail.ru>
2026-03-15 17:35:36 +0500

    Генератор паролец v2.0

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
 "use strict"
 const rl = require("readline-sync");
+function question(answer){
+    if(answer == "нет") return false;
+    return true;
+}
+
+const options = {
+        length: (function passLength() {
+            const passLen = Number(parseInt(rl.question("Введите длину пароля: ")));
+            if(!isNaN(passLen)) return passLen;
+            return passLength();
+        })(),
+        useUppercase: (() => {
+            let answer = rl.question(`Использовать верхний регистр?(по умолчанию ДА)\nда/нет: `).toLowerCase();
+            return question(answer);
+        })(),
+        useNumbers: (() => {
+            let answer = rl.question(`Использовать цифры?(по умолчанию ДА)\nда/нет: `).toLowerCase();
+            return question(answer);
+        })(),
+    };
 
 function passwordGenerator() {
-    const passLen = parseInt(rl.question("Введите длину пароля: "));
-    const lowercaseCharacters = 'abcdefghijklmnopqrstuvwxyz1234567890'.split('');
-    if(isNaN(passLen)) {
-        console.log("Вы ввели некорректную длину!");
-        console.log(passwordGenerator());
-    }
+    let chars = 'abcdefghijklmnopqrstuvwxyz';
+    if(options.useUppercase) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if(options.useNumbers) chars += "1234567890"
+    let passLen = options.length;
     let password = [];
     for(let i = 0; i < passLen; i++) {
-        let randomCharacter = lowercaseCharacters[Math.floor(Math.random() *
-            lowercaseCharacters.length)]
-        let randomFloat = Math.random(); // создаем рандомную переменную для определения будет ли буква заглавной
-        if (randomFloat > 0.5) {
-            password.push(randomCharacter.toUpperCase());
-        }
-        else {
-            password.push(randomCharacter);
-        }
+        let randomCharacter = chars[Math.floor(Math.random() *
+            chars.length)]
+        password.push(randomCharacter);
     }
     return password.join("");
 }
